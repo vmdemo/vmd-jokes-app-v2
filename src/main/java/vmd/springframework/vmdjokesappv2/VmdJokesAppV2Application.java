bacab7aca6655d118a9eaf7cdbc69918867d342c
vmdemo <7242895238@online.de>
2022-06-29 23:34:50 +0200

    Create Project.

---
+++ src/main/java/vmd/springframework/vmdjokesappv2/VmdJokesAppV2Application.java
@@ -0,0 +1,13 @@
+package vmd.springframework.vmdjokesappv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VmdJokesAppV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VmdJokesAppV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/vmd/springframework/vmdjokesappv2/VmdJokesAppV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package vmd.springframework.vmdjokesappv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VmdJokesAppV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
