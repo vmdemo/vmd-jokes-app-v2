be819f2739fa441d0ecef1f6f33810e71a1ec59e
vmdemo <7242895238@online.de>
2022-07-04 20:30:33 +0200

    Create View . Closes #1

---
+++ src/main/java/vmd/springframework/spring5jokesappv2/controllers/JokeController.java
@@ -0,0 +1,24 @@
+package vmd.springframework.spring5jokesappv2.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import vmd.springframework.spring5jokesappv2.services.JokeService;
+
+@Controller
+public class JokeController {
+
+    private final JokeService jokeService;
+
+    // @Autowired
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/", ""})
+    public String showJoke (Model model) {
+        model.addAttribute("joke", jokeService.getJoke());
+        return "index";
+    }
+}
